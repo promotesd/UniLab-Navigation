c2166511d518c2b55e6e029e702f6cb6bae2acc4
xiaodudu <1412822254@qq.com>
2026-07-15 22:37:18 +0800

    feat(navigation): add vectorized diff-drive kinematics

--- src/unilab/envs/navigation/diff_drive/__init__.py
+++ src/unilab/envs/navigation/diff_drive/__init__.py
@@ -1,5 +1,10 @@
 """Differential-drive navigation tasks."""
 
+from .kinematics import differential_drive_step, wrap_angle
 from .point_goal_cfg import DiffDrivePointGoalCfg
 
-__all__ = ["DiffDrivePointGoalCfg"]
+__all__ = [
+    "DiffDrivePointGoalCfg",
+    "differential_drive_step",
+    "wrap_angle",
+]
